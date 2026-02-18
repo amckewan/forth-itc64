3243c8b14775cb80c11954b2b088c1ac81fb2892
Andrew McKewan <andrew.mckewan@gmail.com>
2026-02-18 06:11:23 -0500

    Improve .WID

--- src/search-order.f
+++ src/search-order.f
@@ -1,27 +1,25 @@
 ( Search-order and vocabularies )
 
-\ wordlist:   | last xt | voc-link | nfa or 0 |
+\ wordlist:   | last xt | voc-link |
 VARIABLE VOC-LINK ( points to the most-recently-created wordlist )
+: WORDLIST, ( -- )
+    HERE  0 , ( wordlist )  VOC-LINK @ ,  VOC-LINK ! ;
+
 : WORDLIST ( -- wid )
-    ALIGN  HERE  0 , ( last xt )
-    DUP VOC-LINK  DUP @ ,  ! ( link )
-    0 , ( no name ) ;
+    ALIGN  0 , ( no name )  HERE  WORDLIST, ;
 
 : VOCABULARY ( -- ) \ named wordlist
-    CREATE  WORDLIST  LAST @  SWAP 2 CELLS + ! ( has a name )
-    DOES>  CONTEXT ! ;
+    CREATE  WORDLIST,  DOES>  CONTEXT ! ;
 
 VOCABULARY FORTH
 ' FORTH >BODY CONSTANT FORTH-WORDLIST
 
 : GET-ORDER ( -- widn ... wid1 n )
-    0 ( n)  0 7 DO   I CELLS CONTEXT + @  ?DUP IF SWAP 1+ THEN  -1 +LOOP ;
-
+    0 ( n)  0 7 DO  I CELLS CONTEXT + @  ?DUP IF SWAP 1+ THEN  -1 +LOOP ;
 : SET-ORDER ( widn ... wid1 n -- )
-    CONTEXT 8 CELLS ERASE
-    0 ?DO  I CELLS CONTEXT + !  LOOP ;
+    CONTEXT 8 CELLS ERASE  0 ?DO  I CELLS CONTEXT + !  LOOP ;
 
-: GET-CURRENT   CURRENT @ ; ( ugh )
+: GET-CURRENT   CURRENT @ ;
 : SET-CURRENT   CURRENT ! ;
 : DEFINITIONS   CONTEXT @  SET-CURRENT ;
 
@@ -29,7 +27,9 @@ VOCABULARY FORTH
 : ALSO          GET-ORDER  OVER SWAP 1+  SET-ORDER ;
 : PREVIOUS      GET-ORDER  NIP       1-  SET-ORDER ;
 
-: .WID ( wid -- )  DUP 2 CELLS + @ ?DUP IF  .NFA DROP  ELSE  .  THEN ;
+: .WID ( wid -- )
+    DUP 1 CELLS - @ IF ( vocabulary ) [ 2 CELLS 5 + ] LITERAL - .NFA
+    ELSE ( wordlist ) .  THEN ;
 
 : ORDER ." Context: " GET-ORDER 0 ?DO  .WID  LOOP
         ." Current: " GET-CURRENT .WID ;
